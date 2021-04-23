b6b877abc44014652c2300a4e8af47c2340b7699
delphiets <delfiyets@gmail.com>
2021-04-23 13:47:23 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#505584]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,18 +7,22 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
 
-    private static int  index = 1;
+    private static int index = 1, count;
 
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements value = doc.select(".altCol");
-        for (Element el : row) {
-            Element href = el.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(value.get(index).text());
-            index += 2;
+        while (count <= 5) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers" + "/" + count).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements value = doc.select(".altCol");
+            for (Element el : row) {
+                Element href = el.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(value.get(index).text());
+                index += 2;
+            }
+            count++;
+            index = 1;
         }
     }
 }
