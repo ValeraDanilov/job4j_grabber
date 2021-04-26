860f647c8fd7e284127de0aa544e9b1c08e6e88f
delphiets <delfiyets@gmail.com>
2021-04-26 20:02:01 +0300

    2.3. Загрузка деталей поста. [#505586]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,12 +4,19 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Post;
+
+import java.io.IOException;
 
 public class SqlRuParse {
 
     private static int index = 1, count;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+        new SqlRuParse().fillInPost();
+    }
+
+    public void outAllJob() throws IOException {
         while (count <= 5) {
             Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers" + "/" + count).get();
             Elements row = doc.select(".postslisttopic");
@@ -24,5 +31,16 @@ public class SqlRuParse {
             count++;
             index = 1;
         }
+    }
+
+    public void fillInPost() throws IOException {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t").get();
+        Elements row = doc.select(".msgBody");
+        Elements row1 = doc.select(".msgFooter");
+        Post post = new Post();
+        post.setText(row.get(1).text());
+        post.setCreated(row1.get(0).text().split("\\[")[0]);
+        System.out.println(post.getText());
+        System.out.println(post.getCreated());
     }
 }
